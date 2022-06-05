7ef0413310166f9bd49c644a38a1c7cff87e1215
atishek <atishekkumar19@gmail.com>
2022-06-05 20:38:06 +0530

    Added abstract syntax tree node definitions

---
+++ app/src/main/kotlin/yellow/Expr.kt
@@ -0,0 +1,129 @@
+package yellow
+
+abstract class Expr {
+  interface Visitor<T> {
+    fun visitBinaryExpr(expr: Binary): T
+    fun visitAssignExpr(expr: Assign): T
+    fun visitCallExpr(expr: Call): T
+    fun visitGetExpr(expr: Get): T
+    fun visitGroupingExpr(expr: Grouping): T
+    fun visitLiteralExpr(expr: Literal): T
+    fun visitLogicalExpr(expr: Logical): T
+    fun visitSetExpr(expr: Set): T
+    fun visitThisExpr(expr: This): T
+    fun visitUnaryExpr(expr: Unary): T
+    fun visitVariableExpr(expr: Variable): T
+  }
+
+  // Assignment expression: <name: Token> = <value: Expr>
+  class Assign(
+      val name: Token,
+      val value: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitAssignExpr(this)
+    }
+  }
+
+  // Binary Expression: <left:Expr> <op:Token> <right:Expr>
+  class Binary(
+      val left: Expr,
+      val operator: Token,
+      val right: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitBinaryExpr(this)
+    }
+  }
+
+  // Function call expression: <callee: Expr>(<args: List<Expr>)
+  class Call(
+      val callee: Expr,
+      val paren: Token, // ending right parenthesis - For error reporting
+      val args: List<Expr>,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitCallExpr(this)
+    }
+  }
+
+  // Object property getter: <Object: Expr>.<name:Token>
+  class Get(
+      val obj: Expr,
+      val name: Token,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitGetExpr(this)
+    }
+  }
+
+  // Parenthesis (<expr: Expr>)
+  class Grouping(
+      val expr: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitGroupingExpr(this)
+    }
+  }
+
+  // Number, string, boolean literal (nil as well)
+  class Literal(
+      val value: Any?,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitLiteralExpr(this)
+    }
+  }
+
+  // Logical operator: and/or (short-circuited)
+  class Logical(
+      val left: Expr,
+      val operator: Token,
+      val right: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitLogicalExpr(this)
+    }
+  }
+
+  // Object property setter: <Object:Expr>.<name: Token> = <value: Expr>
+  class Set(
+      val obj: Expr,
+      val name: Token,
+      val value: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitSetExpr(this)
+    }
+  }
+
+  // this keyword for referencing instance in methods
+  class This(
+      val keyword: Token,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitThisExpr(this)
+    }
+  }
+
+  // Unary operator <op: Token> <value: Expr>
+  class Unary(
+      val operator: Token,
+      val right: Expr,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitUnaryExpr(this)
+    }
+  }
+
+  // Accessing variable bindings
+  class Variable(
+      val name: Token,
+  ) : Expr() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitVariableExpr(this)
+    }
+  }
+
+  abstract fun <T> accept(visitor: Visitor<T>): T
+}
---
+++ app/src/main/kotlin/yellow/Stmt.kt
@@ -0,0 +1,103 @@
+package yellow
+
+abstract class Stmt {
+  interface Visitor<T> {
+    fun visitBlockStmt(stmt: Block): T
+    fun visitClassStmt(stmt: Class): T
+    fun visitFunctionStmt(stmt: Function): T
+    fun visitExpressionStmt(stmt: Expression): T
+    fun visitIfStmt(stmt: If): T
+    fun visitPrintStmt(stmt: Print): T
+    fun visitReturnStmt(stmt: Return): T
+    fun visitVarStmt(stmt: Var): T
+    fun visitWhileStmt(stmt: While): T
+  }
+
+  // Block statements: {<block: List<Stmt>}
+  class Block(
+      val statements: List<Stmt>,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitBlockStmt(this)
+    }
+  }
+
+  // Class node
+  class Class(
+      val name: Token,
+      val methods: List<Stmt.Function>,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitClassStmt(this)
+    }
+  }
+
+  // Expression statements - expression + ';'
+  class Expression(
+      val expression: Expr,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitExpressionStmt(this)
+    }
+  }
+
+  // Function Declaration
+  class Function(
+      val name: Token,
+      val params: List<Token>,
+      val body: List<Stmt>,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitFunctionStmt(this)
+    }
+  }
+
+  // if(<condition: Expr>) <thenBranch:Stmt> else <elseBranch:Stmt>
+  class If(
+      val condition: Expr,
+      val thenBranch: Stmt,
+      val elseBranch: Stmt?,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitIfStmt(this)
+    }
+  }
+
+  // Debug print stmt
+  class Print(
+      val expression: Expr,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitPrintStmt(this)
+    }
+  }
+
+  class Return(
+      val keyword: Token,
+      val value: Expr?,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitReturnStmt(this)
+    }
+  }
+
+  class Var(
+      val name: Token,
+      val value: Expr?,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitVarStmt(this)
+    }
+  }
+
+  class While(
+      val condition: Expr,
+      val body: Stmt,
+  ) : Stmt() {
+    override fun <T> accept(visitor: Visitor<T>): T {
+      return visitor.visitWhileStmt(this)
+    }
+  }
+
+  abstract fun <T> accept(visitor: Visitor<T>): T
+}
